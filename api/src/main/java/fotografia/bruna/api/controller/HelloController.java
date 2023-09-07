cf0757a1cabeab47abbb5684e6d984c4fcb46def
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-09-07 13:34:17 -0300

    add: comentários sobre controllers

--- api/src/main/java/fotografia/bruna/api/controller/HelloController.java
+++ api/src/main/java/fotografia/bruna/api/controller/HelloController.java
@@ -5,12 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// para trabalhar com web tradicional, podemos usar @Controller ao invés de @RestController
+// Para trabalhar com web tradicional, podemos usar @Controller ao invés de @RestController
+// No MVC, o controller é uma classe que mapeia as requisições e respostas da API. É como se fosse uma rota.
 // Controller é uma classe que recebe requisições e devolve respostas
-@RestController // anotação que indica que é uma classe de controle do Spring Boot
-@RequestMapping("/hello") // indica que a classe vai receber requisições na rota /hello
+@RestController // Anotação que indica que é uma classe de controle do Spring Boot
+@RequestMapping("/hello") // Indica que a classe vai receber requisições na rota /hello
 public class HelloController {
-    @GetMapping // indica que o método vai receber requisições do tipo GET e devolver uma resposta
+    @GetMapping // Indica que o método vai receber requisições do tipo GET e devolver uma resposta
     public String hello() {
         return "Hello World!";
     }
