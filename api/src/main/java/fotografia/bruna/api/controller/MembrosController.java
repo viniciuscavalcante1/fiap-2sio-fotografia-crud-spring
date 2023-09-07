c1322345b5a5888fd8ebad433fb5fbcdc6d61a06
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-09-07 19:27:08 -0300

    fix: ajusta MembrosController para ser um método transacional

--- api/src/main/java/fotografia/bruna/api/controller/MembrosController.java
+++ api/src/main/java/fotografia/bruna/api/controller/MembrosController.java
@@ -4,6 +4,7 @@ import fotografia.bruna.api.membro.DadosCadastroMembro;
 import fotografia.bruna.api.membro.Membro;
 import fotografia.bruna.api.membro.MembroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ public class MembrosController {
     private MembroRepository repository;
 
     @PostMapping
+    @Transactional // Anotação do Spring para indicar que o método é transacional
     // Esse é o padrão de projeto chamado DTO (Data Transfer Object)
     public void cadastrarMembro(@RequestBody DadosCadastroMembro dados) { // @RequestBody indica que o método vai receber um JSON no corpo da requisição
         repository.save(new Membro(dados)); // Salva o membro no banco de dados
