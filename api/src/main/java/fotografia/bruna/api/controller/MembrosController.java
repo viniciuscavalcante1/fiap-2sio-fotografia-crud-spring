1f3e4d135b8d2d598e1b1dd59fdc42706e5a9608
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-09-08 19:06:24 -0300

    add: detalhar membro

--- api/src/main/java/fotografia/bruna/api/controller/MembrosController.java
+++ api/src/main/java/fotografia/bruna/api/controller/MembrosController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -42,5 +43,11 @@ public class MembrosController {
     @Transactional
     public void excluirMembro(@PathVariable Long id) {
         repository.deleteById(id);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity detalharMembro(@PathVariable Long id) {
+        var membro = repository.getReferenceById(id);
+        return ResponseEntity.ok(new DadosDetalhesMembro(membro));
     }
 }
