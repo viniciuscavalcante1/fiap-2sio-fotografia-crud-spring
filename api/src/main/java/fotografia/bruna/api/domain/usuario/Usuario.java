595af2110de6e8a0512f37dcdfc34343fd50c972
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-09-09 18:39:54 -0300

    add: usuarios migration

---
+++ api/src/main/java/fotografia/bruna/api/domain/usuario/Usuario.java
@@ -0,0 +1,19 @@
+package fotografia.bruna.api.domain.usuario;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+}
