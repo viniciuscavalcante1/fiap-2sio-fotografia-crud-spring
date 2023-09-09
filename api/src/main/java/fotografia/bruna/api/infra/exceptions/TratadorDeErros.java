8569dd0cb26a9148db17da5f80bb457f9f0a8ad0
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-09-09 19:02:07 -0300

    add: security configurations

--- api/src/main/java/fotografia/bruna/api/infra/exceptions/TratadorDeErros.java
+++ api/src/main/java/fotografia/bruna/api/infra/exceptions/TratadorDeErros.java
@@ -1,4 +1,4 @@
-package fotografia.bruna.api.infra;
+package fotografia.bruna.api.infra.exceptions;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
