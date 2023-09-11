6164ab5b35010c7f359bc9f62f49e7472f74e93e
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-09-11 17:29:43 -0300

    cliente: muda de rest pra html

--- api/src/main/java/fotografia/bruna/api/controller/ClienteController.java
+++ api/src/main/java/fotografia/bruna/api/controller/ClienteController.java
@@ -1,34 +1,57 @@
 package fotografia.bruna.api.controller;
 
 import fotografia.bruna.api.domain.cliente.*;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
 @RequestMapping("/clientes")
-@SecurityRequirement(name = "bearer-key")
 public class ClienteController {
 
     @Autowired
     private ClienteRepository repository;
 
-    @PostMapping
-    @Transactional
-    public void cadastrarCliente(@RequestBody @Valid DadosCadastroCliente dados) {
-        repository.save(new Cliente(dados));
-    }
+
 
     @GetMapping
-    public Page<DadosListagemCliente> listarCliente(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
-        return repository.findAll(paginacao).map(DadosListagemCliente::new);
+    public String listarCliente(Model model) {
+        List<DadosListagemCliente> clientes = repository.findAll().stream().map(DadosListagemCliente::new).collect(Collectors.toList());
+        model.addAttribute("clientes", clientes);
+        return "lista-clientes";
     }
+
+    @GetMapping("/cadastrar")
+    public String viewCadastrarCliente(Model model) {
+        System.out.println("Exibindo formulário de cadastro de cliente");
+        model.addAttribute("cliente", new DadosCadastroCliente("", "", "", ""));
+        return "cadastro-cliente";
+    }
+
+    @PostMapping("/cadastrar")
+    public String cadastrarCliente(Model model, @ModelAttribute("cliente") DadosCadastroCliente cliente) {
+        System.out.println("Nome: " + cliente.nome());
+        repository.save(new Cliente(cliente));
+        model.addAttribute("cliente", cliente);
+        return "redirect:/clientes"; // Nome do template HTML para o formulário de cadastro
+    }
+
+
+
+    // @PostMapping("/cadastrar")
+    // @Transactional
+    // public void cadastrarCliente(@RequestBody @Valid DadosCadastroCliente dados) {
+    //     System.out.println("Nome: ");
+    //     Cliente cliente = repository.save(new Cliente(dados));
+    // }
 
     @PutMapping
     @Transactional
--- api/src/main/java/fotografia/bruna/api/domain/cliente/DadosCadastroCliente.java
+++ api/src/main/java/fotografia/bruna/api/domain/cliente/DadosCadastroCliente.java
@@ -5,16 +5,9 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public record DadosCadastroCliente(
-        @NotBlank
         String nome,
-
-        @NotBlank @Email
         String email,
-
-        @NotBlank @Pattern(regexp = "\\d{10,11}")
         String cpf,
-
-        @NotBlank @Pattern(regexp = "\\d{8,13}")
         String telefone
 ) {
 
